5966dd6786424ef47634197a89307ad1daf13632
Temirbek <sydykovm@gmail.com>
2023-11-22 13:48:34 +0600

    gacm

--- views/layouts/index.php
+++ views/layouts/index.php
@@ -16,8 +16,24 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 
 <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52342776-1" type="279bf399d1449e8ce1263309-text/javascript"></script>
+    <script type="279bf399d1449e8ce1263309-text/javascript">
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-52342776-1');
+        gtag('config', 'AW-1001497063');
+    </script>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="
+Разработка веб-сайтов, интернет-магазинов и мобильных приложений (iOS, Android) в Бишкеке, в Кыргызстане. Веб-студия в Бишкеке. Дизайн и создание сайтов и приложений под ключ. Заказать сайт визитку. Заказать разрботку мобильного приложения (iOS, Android, Андроид) 
+">
+    <meta name="keywords" content="разработка сайтов веб-сайтов мобильных приложений iOS Android интернет магазин интернет-магазин Бишкек Кыргызстан в Бишкеке в Кыргызстане дизайн сайта приложения веб-студия лучшие веб-студии программисты услуги программистов веб-разработка yii разработчики заказать сайт">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
